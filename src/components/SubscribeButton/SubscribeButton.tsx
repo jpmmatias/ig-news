a03aec4ac438d832001e6d091fbf4cc13409290c
João Pedro Matias <joao.matias@rebase.com.br>
2022-03-08 14:56:10 -0300

    Finalizando pagamento stripe

--- src/components/SubscribeButton/SubscribeButton.tsx
+++ src/components/SubscribeButton/SubscribeButton.tsx
@@ -1,10 +1,43 @@
 import styles from './style.module.scss';
+import { useSession } from 'next-auth/react';
+import { signIn } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJS } from '../../services/stripe-js';
+
+// Para informações secretas - como secrets de apis
+// getServerSideProps (SSR)
+// getStaticProps (SSG)
+// API Routes - > Melhor para quando depende de execução do usuário, como o clique de um botão
 
 interface SubscriptionProrps {
 	id: string;
 }
 const SubscribeButton = ({ id }: SubscriptionProrps) => {
-	return <button className={styles.subscribeButton}>Subscribe Now</button>;
+	const { data: session } = useSession();
+	const userNotLogged = !session;
+
+	async function handleSubscribe() {
+		if (userNotLogged) {
+			signIn('github');
+			return;
+		}
+
+		try {
+			const response = await api.post('/subscribe');
+			const { sessionId } = response.data;
+			console.log(sessionId);
+			const stripe = await getStripeJS();
+
+			stripe.redirectToCheckout({ sessionId });
+		} catch (error) {
+			alert(error.message);
+		}
+	}
+	return (
+		<button onClick={handleSubscribe} className={styles.subscribeButton}>
+			Subscribe Now
+		</button>
+	);
 };
 
 export default SubscribeButton;
---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,5 @@
+const Posts = () => {
+	return <div>index</div>;
+};
+
+export default Posts;
