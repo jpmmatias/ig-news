a36765e1c6e107161bde1de50133b9f745be58b1
João Pedro Matias <joao.matias@rebase.com.br>
2022-03-08 08:24:58 -0300

    Autenticação com github finalizado

--- src/components/Header/SignInButton/SignInButton.tsx
+++ src/components/Header/SignInButton/SignInButton.tsx
@@ -2,21 +2,26 @@ import React from 'react';
 import style from './style.module.scss';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, useSession, signOut } from 'next-auth/react';
 
 const SignInButton = () => {
-	const isUserLoggedIn = true;
-	if (isUserLoggedIn) {
+	const { data: session } = useSession();
+
+	if (session) {
 		return (
 			<button type='button' className={style.button}>
 				<FaGithub color='#04b361' />
-				<span>João Pedro Matias</span>
-				<FiX color='04b361'></FiX>
+				<span>{session.user.name}</span>
+				<FiX onClick={() => signOut()} color='04b361'></FiX>
 			</button>
 		);
 	}
 
 	return (
-		<button type='button' className={style.button}>
+		<button
+			onClick={() => signIn('github')}
+			type='button'
+			className={style.button}>
 			<FaGithub color='#eba417' />
 			<span>Sign In with Github</span>
 		</button>
