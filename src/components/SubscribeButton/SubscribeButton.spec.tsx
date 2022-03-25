c9e243fb69bb56664a1c167458a255d169aed54c
João Pedro Matias <joao.matias@rebase.com.br>
2022-03-25 09:00:02 -0300

    corrigindo testes

--- src/components/SubscribeButton/SubscribeButton.spec.tsx
+++ src/components/SubscribeButton/SubscribeButton.spec.tsx
@@ -11,6 +11,13 @@ jest.mock('next-auth/react');
 
 describe('SubscribeButton Component', () => {
 	it('render button correctly ', () => {
+		const useSessionMocked = jest.mocked(useSession);
+
+		useSessionMocked.mockReturnValueOnce({
+			data: null,
+			status: 'loading',
+		});
+
 		render(<SubscribeButton />);
 
 		expect(screen.getByText('Subscribe Now')).toBeInTheDocument();
@@ -18,25 +25,29 @@ describe('SubscribeButton Component', () => {
 
 	describe('when user not authenticated', () => {
 		it('redirects user to sign in', () => {
-			render(<SubscribeButton />);
-
 			const useSessionMocked = jest.mocked(useSession);
-			useSessionMocked.mockReturnValueOnce({ data: null, status: 'loading' });
+
+			// Alterei o status para `unauthenticated` pois no seu código você utilizou esse status para saber se o usuário está ou não logado
+			useSessionMocked.mockReturnValueOnce({
+				data: null,
+				status: 'unauthenticated',
+			});
+
+			//Fiz o render somente após os mocks
+			render(<SubscribeButton />);
 
 			const subscribeButton = screen.getByText('Subscribe Now');
 
 			fireEvent.click(subscribeButton);
 
 			const mockedSignIn = jest.mocked(signIn);
-
 			expect(mockedSignIn).toHaveBeenCalled();
 		});
 	});
 
 	describe('when user is authenticated', () => {
 		it('redirect user to posts', () => {
-			render(<SubscribeButton />);
-
+			//Removi o render do começo do arquivo e deixei os mocks em primeiro
 			const useSessionMocked = jest.mocked(useSession);
 			const useRouterMocked = jest.mocked(useRouter);
 
@@ -46,6 +57,7 @@ describe('SubscribeButton Component', () => {
 				data: {
 					user: { name: 'John Doe', email: 'john.doe@example.com' },
 					expires: 'fake-expires',
+					activeSubscription: 'fake-subscription', //Adicionei o mock da subscription
 				},
 				status: 'authenticated',
 			});
@@ -54,13 +66,16 @@ describe('SubscribeButton Component', () => {
 				push: pushMock,
 			} as any);
 
+			// Fiz o render somente após os mocks
+			render(<SubscribeButton />);
+
+			// Removi o mock da função `signIn` pois ela não é utilizada nesse teste
+
 			const subscribeButton = screen.getByText('Subscribe Now');
 
 			fireEvent.click(subscribeButton);
-			const mockedSignIn = jest.mocked(signIn);
 
-			expect(mockedSignIn).toHaveBeenCalled();
-
+			// Removi o expect da função de `signIn` pois ela também não é utilizada nesse teste
 			expect(pushMock).toHaveBeenCalled();
 		});
 	});
